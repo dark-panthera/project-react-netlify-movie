8c14b089e368ed56c344984e2a60336a42c7e049
panthera-dark <panthera.dark@hotmail.com>
2020-03-28 16:13:30 +0100

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
+
 import configureStore from "./store/configureStore";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
