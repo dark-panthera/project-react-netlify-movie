245f326bf6d098cbfa799e11e747fc5453ac26d1
panthera-dark <panthera.dark@hotmail.com>
2020-03-29 05:34:16 +0200

    authentication

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import AppRouter from "./routers/AppRouter";
-import { startSetExpenses } from './actions/expenses';
+import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
+import { startSetExpenses } from "./actions/expenses";
+import {login, logout} from './actions/auth';
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
-import "./firebase/firebase";
+import { firebase } from "./firebase/firebase";
 
 const store = configureStore();
 
@@ -17,8 +18,30 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById("app"));
+    hasRendered = true;
+  }
+};
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById("app"));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    store.dispatch(login(user.uid));
+    console.log("log in");
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else {
+    store.dispatch(logout());
+    console.log("log out");
+    renderApp();
+    history.push("/");
+  }
 });
